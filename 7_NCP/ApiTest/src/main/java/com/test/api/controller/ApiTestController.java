a5139c9a580e9304dde09dc200d55db74b6e91ef
yihanseul <ihanseul731@gmail.com>
2022-06-24 17:59:31 +0900

    api Test

--- 7_NCP/ApiTest/src/main/java/com/test/api/controller/ApiTestController.java
+++ 7_NCP/ApiTest/src/main/java/com/test/api/controller/ApiTestController.java
@@ -15,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.test.api.common.Cfr;
+import com.test.api.common.Cfr2;
 import com.test.api.common.Ocr;
+import com.test.api.common.Speech;
 import com.test.api.common.TextTrans;
 
 @Controller
@@ -52,5 +55,37 @@ public class ApiTestController {
 		
 		model.addAttribute("res",res);
 		return "ocrRes";
+	}
+	
+	@GetMapping("/cfr")
+	public String cfrTest(Model model) throws IOException {
+		String path=Path.of(resourceLoader.getResource("classpath:static").getURI()).toString();
+		
+		Cfr cfr=new Cfr();
+		String res= cfr.cfrTest(path);
+		model.addAttribute("res",res);
+		return "cfrRes";
+	}
+	
+	@GetMapping("/cfr2")
+	public String cfrTest2(Model model) throws IOException {
+		String path=Path.of(resourceLoader.getResource("classpath:static").getURI()).toString();
+		
+		Cfr2 cfr2=new Cfr2();
+		String res=cfr2.cfr2Test(path+"\\img06.jpg");
+		String res1=cfr2.cfr2Test(path+"\\img05.jpg");
+		model.addAttribute("res",res);
+		//model.addAttribute("res1",res1);
+		return "cfrRes2";
+	}
+	
+	@GetMapping("/speech")
+	public String speechTest(Model model) throws IOException {
+		String path=Path.of(resourceLoader.getResource("classpath:static").getURI()).toString();
+		
+		Speech speechTest=new Speech();
+		String res=speechTest.sppechTest(path);
+		model.addAttribute("res",res);
+		return "speechRes";
 	}
 }
